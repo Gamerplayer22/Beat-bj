da64d4ed3a98151e261a6856938e2543b7275eb3
Gamerplayer22 <kalman.lengyel@trojans.dsu.edu>
2026-02-23 18:29:01 -0600

    rethought mechanics update

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 import deck
 
-if __name__=='__main__':
-    num_players = 0
-    num_decks = 0
+# gives the true count
+def true_count(running_count,remaining_shoe_count):
+    return running_count/(remaining_shoe_count/52)
+        
 
-    num_players = int(input('Number of players: '))
+if __name__=='__main__':
+    # how many decks are in the shoe
     num_decks = int(input('Number of decks used: '))
 
-    card = []
+    d ={}
+    running_count = 0
+    # how many cards are left in the shoe
+    remaining_shoe_count = num_decks * 52
 
-    for j in range(2):
-        for i in range(num_players):
-            card.append(input('player ' + str(i+1) + ': '))
-        card.append(input('dealer: '))
 
-    
+    for i in range(len(deck.deck)):
+        d.update({deck.deck[i] : num_decks})
+
+
+    while(1):
+        card = input('Card: ')
+        d[card] -= 1
+        if(card in ['A', 'K', 'Q', 'J', 'T']):
+            running_count -= 1
+        elif(card in ['2', '3', '4', '5', '6']):
+            running_count += 1 
