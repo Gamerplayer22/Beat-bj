cda89453e4d6f655864ab7e0f8485b86c1208d64
Gamerplayer22 <kalman.lengyel@trojans.dsu.edu>
2026-02-28 14:39:22 -0600

    3 of a kind added

--- main.py
+++ main.py
@@ -13,13 +13,16 @@ if __name__=='__main__':
     running_count = 0
     # how many cards are left in the shoe
     remaining_shoe_count = num_decks * 52
+    # keeping track of the suits of the cards
     Hearts = num_decks * 13
     Diamonds = num_decks * 13
     Spades = num_decks * 13
     Clubs = num_decks * 13
+    # keeping track of the count of value cards
+    array = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] # 2, 3, 4, 5, 6, 7, 8, 9, T, J, Q, K, A
+    card_array = [(x + 4)*num_decks for x in array]
 
-    print(Hearts)
-
+    # build dictionary for the shoe
     for i in range(len(deck.deck)):
         d.update({deck.deck[i] : num_decks})
 
@@ -29,6 +32,7 @@ if __name__=='__main__':
         if d[card] != 0:
             d[card] -= 1
             remaining_shoe_count -= 1
+            # updating the suit count
             if card[0] == 'H':
                 Hearts -= 1
             elif card[0] == 'D':
@@ -37,10 +41,24 @@ if __name__=='__main__':
                 Spades -= 1
             else:
                 Clubs -= 1
+            # updating running count
             if card[1] in ['A', 'K', 'Q', 'J', 'T']:
                 running_count -= 1
             elif card[1] in ['2', '3', '4', '5', '6']:
                 running_count += 1
+            # updating value counts
+            if card[1] in ['2', '3', '4', '5', '6', '7', '8' , '9']:
+                card_array[int(card[1])+1] -= 1
+            elif card[1] == 'T':
+                card_array[8] -= 1
+            elif card[1] == 'J':
+                card_array[9] -= 1
+            elif card[1] == 'Q':
+                card_array[10] -= 1
+            elif card[1] == 'K':
+                card_array[11] -= 1
+            elif card[1] == 'A':
+                card_array[12] -= 1
         print('count: ',true_count(running_count,remaining_shoe_count))
-        print(Hearts)
         print(sidebets.flush(Hearts,Diamonds,Spades,Clubs,remaining_shoe_count))
+        print(sidebets.trips(card_array,remaining_shoe_count))
--- sidebets.py
+++ sidebets.py
@@ -24,4 +24,32 @@ def flush(h, d, s, c, remaining_cards):
     else:
         t = 'clubs'
     
-    return print(f'{t}: {max*100}%')
+    return print(f'{t}: {max*100}% , total: {(probability[0]+probability[1]+probability[2]+probability[3])*100}%')
+
+# straight
+def straight():
+    return
+
+# 3 of a kind
+def trips(card_array, remaining_cards):
+                  # 2    3    4    5    6    7    8    9    T    J    Q    K    A   
+    probability = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    for i in range(len(probability)):
+        probability[i] = (card_array[i]/remaining_cards) * ((card_array[i]-1)/(remaining_cards-1)) * ((card_array[i]-2)/(remaining_cards-2))
+    
+    max = probability[0]
+    total = 0
+    for i in range(len(probability)):
+        if probability[i] > max:
+            max = probability[i]
+        total += probability[i]
+
+    return print(f'total: {total*100}%')
+
+# straight flush
+def s_f():
+    return
+
+# royal flush
+def r_f():
+    return
